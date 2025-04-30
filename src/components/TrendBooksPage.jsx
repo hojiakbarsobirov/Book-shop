4ace0d956a4fcd4e24b7df2bf1c12330f9fd90b0
Hojiakbar <hojiakbarsobirov30@gmail.com>
2025-04-30 18:37:39 +0500

    Saytning trend books sahifasi tayyorlanmoqda

--- src/components/TrendBooksPage.jsx
+++ src/components/TrendBooksPage.jsx
@@ -23,11 +23,11 @@ const TrendBooksPage = () => {
     return (
         <section className=' w-full h-[50vh] mt-5 flex justify-center items-center overflow-x-scroll gap-10'>
             {data.map((item) => (
-                <div className='border-2 border-dashed w-[200px] h-[260px] border-black p-2 py-2 rounded-lg'>
-                    <img className='w-full h-[80%]' src={item.img} alt="" />
-                    <div className='w-full flex justify-between '>
-                        <h2>{item.name}</h2>
-                        <p>{item.price}</p>
+                <div className=' w-[230px] hover:scale-105 transition ease-in-out h-[300px] p-2 py-2 rounded-xl'>
+                    <img className='w-full h-[80%] mb-5' src={item.img} alt="" />
+                    <div className='w-full flex justify-between px-2 '>
+                        <h2 className='font-medium'>{item.name}</h2>
+                        <p className='text-green-400'>{item.price}</p>
                     </div>
                 </div>
             ))}
